41995c8eedfdfa093622c51021e99a99f139e6f1
Dustin Brown <dustin@dolthub.com>
2021-04-26 10:00:40 -0700

    /{gen_release_notes.pl, install-deps.sh}: format notes to include dependency repo names

--- install-deps.sh
+++ install-deps.sh
@@ -3,3 +3,4 @@
 perl -MCPAN -e 'CPAN::Shell->rematein("notest", "install", "DateTime::Format::ISO8601")'
 perl -MCPAN -e 'CPAN::Shell->rematein("notest", "install", "Getopt::Long")'
 perl -MCPAN -e 'CPAN::Shell->rematein("notest", "install", "JSON::Parse")'
+perl -MCPAN -e 'CPAN::Shell->rematein("notest", "install", "Sort::Naturally")'
